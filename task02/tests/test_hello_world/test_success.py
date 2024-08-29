04e4631fd626f79c264fca14bff38b7dc0519305
AmitKumar Choudhary <amitkumar_choudhary@epam.com>
2024-08-30 00:07:51 +0530

    fixed test

--- task02/tests/test_hello_world/test_success.py
+++ task02/tests/test_hello_world/test_success.py
@@ -4,7 +4,7 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), {
+        self.assertEqual(self.HANDLER.lambda_handler(dict(), dict()), {
          "statusCode": 200,
          "message": "Hello from Lambda"
      })
